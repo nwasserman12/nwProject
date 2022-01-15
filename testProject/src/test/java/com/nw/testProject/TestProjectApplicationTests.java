19138c90e041ab0a4c128a88592eba2480188def
nwasserman12 <nickyann0823@aol.com>
2022-01-14 22:09:15 -0600

    Initial Commit

---
+++ testProject/src/test/java/com/nw/testProject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nw.testProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
